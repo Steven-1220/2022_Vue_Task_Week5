0a13cc4930684bbf2832a3cda6a69924110b3a51
Steven Chan <richlife1220@gmail.com>
2022-03-06 14:26:21 +0800

    改成先AJAX再打開 modal(有問題待解決)

--- javascript/components/productInfoModal.js
+++ javascript/components/productInfoModal.js
@@ -1,51 +1,47 @@
 export default {
-
-    props: ['id'],
-    template: '#userProductModal',
-
-    data() {
-        return {
-            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
-            apiPath: 'steven1220',
-            modal: {},
-            product: {},
-            quantity: 1,
-        }
+  props: {
+    productM: {
+      type: Object,
+      default() {
+        return {};
+      },
     },
-    // 監聽 id 的變動，有變動則觸發
-    watch: {
-        id() {
-            this.getProductInfo();
-        }
+  },
+
+  template: "#userProductModal",
+
+  data() {
+    return {
+      apiUrl: "https://vue3-course-api.hexschool.io/v2",
+      apiPath: "steven1220",
+      modal: {},
+      product: {},
+      quantity: 1,
+    };
+  },
+
+  methods: {
+    openModal() {
+      this.modal.show();
     },
-    methods: {
-        openModal() {
-            this.modal.show();
-        },
-        closeModal() {
-            this.modal.hide();
-        },
-        // 取得特定產品資訊
-        getProductInfo() {
-            axios.get(`${this.apiUrl}/api/${this.apiPath}/product/${this.id}`)
-                .then(res => {
-                    console.log(res);
-                    this.product = res.data.product;
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        },
-        // 加入購物車
-        addToCart() {
-            this.$emit('add-cart', this.product.id, this.quantity);
-        }
+    closeModal() {
+      this.modal.hide();
     },
 
-    mounted() {
-        this.modal = new bootstrap.Modal(this.$refs.modalRef, {
-            keyboard: false
-        })
+    // 加入購物車
+    addToCart() {
+      this.$emit("add-cart", this.product.id, this.quantity);
+      // 加入購物車後重置數量
+      setTimeout(() => {
+        this.quantity = 1;
+      }, 1000);
     },
+  },
 
-}
+  mounted() {
+    // 將 modal 與 ref 綁定
+    this.modal = new bootstrap.Modal(this.$refs.modalRef, {
+      keyboard: false,
+    });
+  },
+};
